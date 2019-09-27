75cf355fb1f41ba0197734201d63ba4f12cdef21
Roshan Rao <roshan_rao@berkeley.edu>
2019-09-27 14:33:06 -0700

    add leaderboard

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,9 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='TAPE')
+    return render_template('index.html', title='Tasks Assessing Protein Embeddings - TAPE')
+
+
+@app.route('/leaderboard')
+def leaderboard():
+    return render_template('leaderboard.html', title='Leaderboard - TAPE')
