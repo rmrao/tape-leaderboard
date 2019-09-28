092e610e4f3ee7ede0f006f5728e502addeeb8b5
Roshan Rao <roshan_rao@berkeley.edu>
2019-09-27 17:16:26 -0700

    add tape data in csv file

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,15 @@
 from flask import render_template
 from app import app
+import csv
+from pathlib import Path
+
+data_dir = Path(__file__).parent / 'data'
+
+with (data_dir / 'results.csv').open() as f:
+    reader = csv.reader(f)
+    fieldnames = next(reader)
+    public_names = next(reader)
+    model_info = [dict(zip(fieldnames, info_line)) for info_line in reader]
 
 
 @app.route('/')
@@ -10,4 +20,6 @@ def index():
 
 @app.route('/leaderboard')
 def leaderboard():
+    leaderboard_fields = ['model', 'date', 'secondary_structure', 'contact_prediction',
+                          'remote_homology', 'fluorescence', 'stability']
     return render_template('leaderboard.html', title='Leaderboard - TAPE')
