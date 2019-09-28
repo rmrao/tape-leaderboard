ed60ebca6753fec2ff3c4b4b77513c9ef903b69f
Roshan Rao <roshan_rao@berkeley.edu>
2019-09-27 18:28:14 -0700

    leaderboard printing working in flask

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,21 @@
+from typing import List, Dict
 from flask import render_template
 from app import app
 import csv
 from pathlib import Path
+from collections import namedtuple
+
+
+Table = namedtuple('Table',
+                   ['fieldnames', 'public_fields', 'description', 'models'])
 
 data_dir = Path(__file__).parent / 'data'
 
 with (data_dir / 'results.csv').open() as f:
     reader = csv.reader(f)
-    fieldnames = next(reader)
-    public_names = next(reader)
-    model_info = [dict(zip(fieldnames, info_line)) for info_line in reader]
+    fieldnames: List[str] = next(reader)
+    public_names: Dict[str, str] = dict(zip(fieldnames, next(reader)))
+    model_info = [dict(zip(fieldnames, info_line)) for info_line in reader]  # type: ignore
 
 
 @app.route('/')
@@ -20,6 +26,20 @@ def index():
 
 @app.route('/leaderboard')
 def leaderboard():
-    leaderboard_fields = ['model', 'date', 'secondary_structure', 'contact_prediction',
+    leaderboard_fields = ['date', 'model', 'secondary_structure', 'contact_prediction',
                           'remote_homology', 'fluorescence', 'stability']
-    return render_template('leaderboard.html', title='Leaderboard - TAPE')
+    table = Table(leaderboard_fields,
+                  [public_names[fn] for fn in leaderboard_fields],
+                  "Overall Results on TAPE Benchmark Tasks.",
+                  model_info)
+    return render_template('leaderboard.html', title='Leaderboard - TAPE', table=table)
+
+
+@app.route('/leaderboard/secondary_structure')
+def secondary_structure():
+    leaderboard_fields = ['date', 'model', 'ss_cb513_3', 'ss_casp12_3', 'ss_ts115_3', 'ss_cb513_8']
+    table = Table(leaderboard_fields,
+                  [public_names[fn] for fn in leaderboard_fields],
+                  "Overall Results on TAPE Benchmark Tasks.",
+                  model_info)
+    return render_template('leaderboard.html', title='Leaderboard - TAPE', table=table)
